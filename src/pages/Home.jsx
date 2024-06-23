3c9cd07d032488fd6602a5a03a5f6adb99770efc
rameem2003 <rameem2019@gmail.com>
2024-06-23 15:49:16 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "../components/common/Image";
+import Flex from "./../components/common/Flex";
 
 const Home = () => {
   return (
@@ -13,6 +14,18 @@ const Home = () => {
       <h1 className=" font-sans font-semibold text-2xl md:text-4xl xl:text-7xl text-white">
         REACT + TAILWIND
       </h1>
+      <p className=" font-sans font-medium text-xl text-white mt-3">
+        Starter Template of React + Tailwind & Other NPM Packages To Build Your
+        Application
+      </p>
+
+      <Flex className="my-4 p-3 bg-white/20 rounded-md">
+        <p className=" font-sans text-lg text-white">
+          https://github.com/rol2022/reactive-starter.git
+        </p>
+
+        <button></button>
+      </Flex>
 
       <a
         href="https://github.com/rol2022/reactive-starter"
