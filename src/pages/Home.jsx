28b611e4f4059698fb3c4625d87576b6d5f8afb5
rameem2003 <rameem2019@gmail.com>
2024-06-23 17:43:51 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "../components/common/Image";
 import Flex from "./../components/common/Flex";
+import { MdOutlineContentCopy } from "react-icons/md";
+import { FaCheck } from "react-icons/fa";
 
 const Home = () => {
+  const [code, setCode] = useState(
+    "git clone https://github.com/rol2022/reactive-starter.git"
+  );
+
+  const [copy, setCopy] = useState(false);
+
+  const getCode = () => {
+    setCopy(true);
+    navigator.clipboard.writeText(code);
+
+    setTimeout(() => {
+      setCopy(false);
+    }, 5000);
+  };
   return (
     <main className="h-[90vh] w-full bg-bgScreen flex items-center justify-center flex-col">
       <Image
-        className=" w-auto sm:w-[50%] md:w-auto mx-auto"
+        className=" w-auto sm:w-[50%] md:w-[30%] 2xl:w-auto mx-auto"
         src="/logo.png"
         alt="logo"
       />
 
-      <h1 className=" font-sans font-semibold text-2xl md:text-4xl xl:text-7xl text-white">
+      <h1 className=" font-sans font-semibold text-2xl md:text-4xl 2xl:text-7xl text-white">
         REACT + TAILWIND
       </h1>
-      <p className=" font-sans font-medium text-xl text-white mt-3">
+      <p
+        className=" font-sans font-medium text-sm 2xl:text-xl
+       text-white mt-2"
+      >
         Starter Template of React + Tailwind & Other NPM Packages To Build Your
         Application
       </p>
 
-      <Flex className="my-4 p-3 bg-white/20 rounded-md">
-        <p className=" font-sans text-lg text-white">
-          https://github.com/rol2022/reactive-starter.git
-        </p>
+      <Flex className="my-4 p-3 items-center gap-2 bg-white/20 rounded-md">
+        <p className=" font-sans text-sm text-white">{code}</p>
 
-        <button></button>
+        <button
+          onClick={getCode}
+          className="p-2 rounded-lg text-white bg-white/40"
+        >
+          {copy ? <FaCheck /> : <MdOutlineContentCopy />}
+        </button>
       </Flex>
 
       <a
         href="https://github.com/rol2022/reactive-starter"
         target="_blank"
-        className=" mt-5 block py-3 px-5 font-sans font-bold text-xl text-white border-[2px] border-white/40 rounded-lg"
+        className=" mt-2 block py-3 px-5 font-sans font-bold text-sm 2xl:text-xl text-white border-[2px] border-white/40 rounded-lg"
       >
         Get The Template
       </a>
 
-      <p className=" font-sans font-medium text-xl text-white mt-10">
+      <p className=" font-sans font-medium text-sm 2xl:text-xl text-white mt-3">
         Check and edit <code>/src</code> folder
       </p>
     </main>
